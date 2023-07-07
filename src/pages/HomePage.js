7eccd5f41ffb672b3ba0c3509a3b115a3ba6ad16
udofritzke <81312361+udofritzke@users.noreply.github.com>
2023-07-07 11:06:00 -0300

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const HomePage = ()=> {
+  return (
+    <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text> Primeiro App </Text>
+    </View>
+  );
+}
+export default HomePage;
